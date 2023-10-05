1da7813c184ef10cd1bab709a7240187713249da
ADmitriev99 <66620372+ADmitriev99@users.noreply.github.com>
2023-10-05 23:10:13 +0300

    Local changes for Allure

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,38 +1,43 @@
+import jdk.jfr.Description;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 public class Tests {
 
+    @Test
     @Tag("First")
-    @Test()
-    void firstTest(){
-        int a=0;
-        int b=2;
-        Assertions.assertTrue(a+b==2);
+    @Description("First Test")
+    void firstTest() {
+        int a = 0;
+        int b = 2;
+        Assertions.assertTrue(a + b == 2);
     }
 
+    @Test
     @Tag("Second")
-    @Test
-    void secondTest(){
-        int a=2;
-        int b=3;
-        Assertions.assertTrue(a*b==6);
+    @Description("Second Test")
+    void secondTest() {
+        int a = 2;
+        int b = 3;
+        Assertions.assertTrue(a * b == 6);
     }
 
+    @Test
     @Tag("Third")
-    @Test
-    void thirdTest(){
-        int a=4;
-        int b=2;
-        Assertions.assertTrue(a/b==2);
+    @Description("Third Test")
+    void thirdTest() {
+        int a = 4;
+        int b = 2;
+        Assertions.assertTrue(a / b == 2);
     }
 
-    @Tag("Forth")
     @Test
-    void forthTest(){
-        int a=5;
-        int b=3;
-        Assertions.assertTrue(a-b==2);
+    @Tag("Fourth")
+    @Description("Fourth Test")
+    void fourthTest() {
+        int a = 5;
+        int b = 3;
+        Assertions.assertTrue(a - b == 2);
     }
 }
